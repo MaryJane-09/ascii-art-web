91e7bd078b3b660f88a43bed3cc835bb6e5bd9fc
MaryJane-09 <nzekwechinaza0613@gmail.com>
2026-07-09 12:03:14 +0100

    working on theme toggle switch

--- main.go
+++ main.go
@@ -28,7 +28,28 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func themeHandler(w http.ResponseWriter, r *http.Request) {
+	theme := r.FormValue("theme")
+	if theme == "" {
+		theme = "light"
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:  "theme",
+		Value: theme,
+		Path:  "/",
+	})
+
+	http.Redirect(w, r, "/ascii-art", http.StatusSeeOther)
+
+}
+
 func asciiArtHandler(w http.ResponseWriter, r *http.Request) {
+
+	theme := "light"
+	if c, err := r.Cookie("theme"); err == nil{
+		theme = c.Value
+	}
 
 	if r.Method != http.MethodGet && r.Method != http.MethodPost {
 		w.WriteHeader(http.StatusMethodNotAllowed)
@@ -36,7 +57,9 @@ func asciiArtHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if r.Method == http.MethodGet {
-		err := tmpl.Execute(w, Data{})
+		err := tmpl.Execute(w, Data{
+			Theme: theme,
+		})
 		if err != nil {
 			fmt.Println("Template error:", err)
 			return
@@ -46,12 +69,12 @@ func asciiArtHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
 		input := r.FormValue("input")
 		bannerName := r.FormValue("banner")
-		theme := r.FormValue("theme")
 
 		if input == "" {
 			w.WriteHeader(http.StatusBadRequest)
 			tmpl.Execute(w, Data{
 				Error: "Input a text",
+				Theme: theme,
 			})
 			return
 		}
@@ -62,6 +85,7 @@ func asciiArtHandler(w http.ResponseWriter, r *http.Request) {
 				Error:  "Select a banner",
 				Input:  input,
 				Banner: bannerName,
+				Theme: theme,
 			})
 			return
 		}
@@ -72,6 +96,7 @@ func asciiArtHandler(w http.ResponseWriter, r *http.Request) {
 			tmpl.Execute(w, Data{
 				Error: err.Error(),
 				Input: input,
+				Theme: theme,
 			})
 			return
 		}
@@ -82,6 +107,7 @@ func asciiArtHandler(w http.ResponseWriter, r *http.Request) {
 			tmpl.Execute(w, Data{
 				Error: "Invalid Banner File. Pick a valid Banner.",
 				Input: input,
+				Theme: theme,
 			})
 			return
 		}
@@ -111,6 +137,7 @@ func main() {
 
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/ascii-art", asciiArtHandler)
+	http.HandleFunc("/theme", themeHandler)
 	fmt.Println("server running at port :8080")
 	http.ListenAndServe(":8080", nil)
 }
